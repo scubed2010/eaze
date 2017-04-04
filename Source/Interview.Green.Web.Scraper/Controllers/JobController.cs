d24a1e8265b8fb30c80a99e0e493ca1607d126bd
Kyle Barnes <Kyle Barnes>
2017-04-04 10:30:51 -0400

    added documentation

--- Source/Interview.Green.Web.Scraper/Controllers/JobController.cs
+++ Source/Interview.Green.Web.Scraper/Controllers/JobController.cs
@@ -1,9 +1,7 @@
 ﻿using Interview.Green.Web.Scraper.Interfaces;
-using Interview.Green.Web.Scraper.Models;
 using Interview.Green.Web.Scraper.Service;
 using Interview.Green.Web.Scrapper.Models;
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web.Http;
 
@@ -19,7 +17,12 @@ namespace Interview.Green.Web.Scraper.Controllers
             _jobSchedulerService = new JobSchedulerService();
             _webScrapeService = new WebScrapeService();
         }
-
+        /// <summary>
+        /// Schedule url and optional selected first element for scraping
+        /// </summary>
+        /// <param name="request.Url">http://www.seattlesoftwaresolutions.com/About</param>
+        /// <param name="request.RequestedElement">h1</param>
+        /// <returns>Job ID as GUID</returns>
         [HttpPost]
         public async Task<IHttpActionResult> ScrapeUrl(JobRequest request)
         {
@@ -28,11 +31,16 @@ namespace Interview.Green.Web.Scraper.Controllers
             return Ok(result);
         }
 
+        /// <summary>
+        /// Returns processing until content is available where it will return JobResult object at JSON
+        /// </summary>
+        /// <param name="requestId">GUID value returned from Quartz scheduling application</param>
+        /// <returns>Status string or JobResult object as JSON </returns>
         [HttpGet]
         public async Task<IHttpActionResult> ScrapeUrlStatus(Guid requestId)
         {
             var isComplete = await Task.Run(() => _jobSchedulerService.IsJobCompete(requestId));
-            var content = _webScrapeService.RetrieveScrapeContent(requestId);
+            var content = await Task.Run(() => _webScrapeService.RetrieveScrapeContent(requestId));
 
             if (isComplete && !string.IsNullOrEmpty(content))
             {
